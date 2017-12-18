ef2ab43421ded7599ca6ad9b812584fa0752cde1
Stephen Amaza <steveamaza@gmail.com>
2017-12-18 18:47:44 +0100

    Payment method shows on checkout after editing model.
    Uncomment Paystack switches NGN on catalog/model

--- 3.0.x/admin/controller/extension/payment/paystack.php
+++ 3.0.x/admin/controller/extension/payment/paystack.php
@@ -35,13 +35,11 @@ class ControllerExtensionPaymentPaystack extends controller
         $this->load->model('setting/setting');
 
         if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-            // echo "Didn't get here";
-            // die();
-            $this->model_setting_setting->editSetting('paystack', $this->request->post);
+            $this->model_setting_setting->editSetting('payment_paystack', $this->request->post);
 
             $this->session->data['success'] = $this->language->get('text_success');
 
-            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token']. '&type=payment', 'SSL'));
+            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token']. '&type=payment', true));
         }
 
         $data['heading_title'] = $this->language->get('heading_title');
@@ -94,107 +92,107 @@ class ControllerExtensionPaymentPaystack extends controller
 
         $data['breadcrumbs'][] = array(
         'text' => $this->language->get('text_home'),
-        'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], 'SSL')
+        'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
         );
 
         $data['breadcrumbs'][] = array(
         'text' => $this->language->get('text_payment'),
-        'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'].'&type=payment', 'SSL')
+        'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'].'&type=payment', true)
         );
 
         $data['breadcrumbs'][] = array(
         'text' => $this->language->get('heading_title'),
-        'href' => $this->url->link('extension/payment/paystack', 'user_token=' . $this->session->data['user_token'], 'SSL')
+        'href' => $this->url->link('extension/payment/paystack', 'user_token=' . $this->session->data['user_token'], true)
         );
 
-        $data['action'] = $this->url->link('extension/payment/paystack', 'user_token=' . $this->session->data['user_token'], 'SSL');
+        $data['action'] = $this->url->link('extension/payment/paystack', 'user_token=' . $this->session->data['user_token'], true);
 
-        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', 'SSL');
+        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true);
 
-        if (isset($this->request->post['paystack_live_secret'])) {
-            $data['paystack_live_secret'] = $this->request->post['paystack_live_secret'];
+        if (isset($this->request->post['payment_paystack_live_secret'])) {
+            $data['payment_paystack_live_secret'] = $this->request->post['payment_paystack_live_secret'];
         } else {
-            $data['paystack_live_secret'] = $this->config->get('paystack_live_secret');
+            $data['payment_paystack_live_secret'] = $this->config->get('payment_paystack_live_secret');
         }
  
-        if (isset($this->request->post['paystack_live_public'])) {
-            $data['paystack_live_public'] = $this->request->post['paystack_live_public'];
+        if (isset($this->request->post['payment_paystack_live_public'])) {
+            $data['payment_paystack_live_public'] = $this->request->post['payment_paystack_live_public'];
         } else {
-            $data['paystack_live_public'] = $this->config->get('paystack_live_public');
+            $data['payment_paystack_live_public'] = $this->config->get('payment_paystack_live_public');
         }
  
-        if (isset($this->request->post['paystack_test_public'])) {
-            $data['paystack_test_secret'] = $this->request->post['paystack_test_secret'];
+        if (isset($this->request->post['payment_paystack_test_secret'])) {
+            $data['payment_paystack_test_secret'] = $this->request->post['payment_paystack_test_secret'];
         } else {
-            $data['paystack_test_secret'] = $this->config->get('paystack_test_secret');
+            $data['payment_paystack_test_secret'] = $this->config->get('payment_paystack_test_secret');
         }
  
-        if (isset($this->request->post['paystack_test_public'])) {
-            $data['paystack_test_public'] = $this->request->post['paystack_test_public'];
+        if (isset($this->request->post['payment_paystack_test_public'])) {
+            $data['payment_paystack_test_public'] = $this->request->post['payment_paystack_test_public'];
         } else {
-            $data['paystack_test_public'] = $this->config->get('paystack_test_public');
+            $data['payment_paystack_test_public'] = $this->config->get('payment_paystack_test_public');
         }
  
-        if (isset($this->request->post['paystack_live'])) {
-            $data['paystack_live'] = $this->request->post['paystack_live'];
+        if (isset($this->request->post['payment_paystack_live'])) {
+            $data['payment_paystack_live'] = $this->request->post['payment_paystack_live'];
         } else {
-            $data['paystack_live'] = $this->config->get('paystack_live');
+            $data['payment_paystack_live'] = $this->config->get('payment_paystack_live');
         }
 
-        if (isset($this->request->post['paystack_debug'])) {
-            $data['paystack_debug'] = $this->request->post['paystack_debug'];
+        if (isset($this->request->post['payment_paystack_debug'])) {
+            $data['payment_paystack_debug'] = $this->request->post['payment_paystack_debug'];
         } else {
-            $data['paystack_debug'] = $this->config->get('paystack_debug');
+            $data['payment_paystack_debug'] = $this->config->get('payment_paystack_debug');
         }
 
-        if (isset($this->request->post['paystack_total'])) {
-            $data['paystack_total'] = $this->request->post['paystack_total'];
+        if (isset($this->request->post['payment_paystack_total'])) {
+            $data['payment_paystack_total'] = $this->request->post['payment_paystack_total'];
         } else {
-            $data['paystack_total'] = $this->config->get('paystack_total');
+            $data['payment_paystack_total'] = $this->config->get('payment_paystack_total');
         }
 
-        if (isset($this->request->post['paystack_approved_status_id'])) {
-            $data['paystack_approved_status_id'] = $this->request->post['paystack_approved_status_id'];
+        if (isset($this->request->post['payment_paystack_approved_status_id'])) {
+            $data['payment_paystack_approved_status_id'] = $this->request->post['payment_paystack_approved_status_id'];
         } else {
-            $data['paystack_approved_status_id'] = $this->config->get('paystack_approved_status_id');
+            $data['payment_paystack_approved_status_id'] = $this->config->get('payment_paystack_approved_status_id');
         }
 
-        if (isset($this->request->post['paystack_declined_status_id'])) {
-            $data['paystack_declined_status_id'] = $this->request->post['paystack_declined_status_id'];
+        if (isset($this->request->post['payment_paystack_declined_status_id'])) {
+            $data['payment_paystack_declined_status_id'] = $this->request->post['payment_paystack_declined_status_id'];
         } else {
-            $data['paystack_declined_status_id'] = $this->config->get('paystack_declined_status_id');
+            $data['payment_paystack_declined_status_id'] = $this->config->get('payment_paystack_declined_status_id');
         }
 
-        if (isset($this->request->post['paystack_error_status_id'])) {
-            $data['paystack_error_status_id'] = $this->request->post['paystack_error_status_id'];
+        if (isset($this->request->post['payment_paystack_error_status_id'])) {
+            $data['payment_paystack_error_status_id'] = $this->request->post['payment_paystack_error_status_id'];
         } else {
-            $data['paystack_error_status_id'] = $this->config->get('paystack_error_status_id');
+            $data['payment_paystack_error_status_id'] = $this->config->get('payment_paystack_error_status_id');
         }
 
         $this->load->model('localisation/order_status');
 
         $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
 
-        if (isset($this->request->post['paystack_geo_zone_id'])) {
-            $data['paystack_geo_zone_id'] = $this->request->post['paystack_geo_zone_id'];
+        if (isset($this->request->post['payment_paystack_geo_zone_id'])) {
+            $data['payment_paystack_geo_zone_id'] = $this->request->post['payment_paystack_geo_zone_id'];
         } else {
-            $data['paystack_geo_zone_id'] = $this->config->get('paystack_geo_zone_id');
+            $data['payment_paystack_geo_zone_id'] = $this->config->get('payment_paystack_geo_zone_id');
         }
 
         $this->load->model('localisation/geo_zone');
 
         $data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
 
-        if (isset($this->request->post['paystack_status'])) {
-            $data['paystack_status'] = $this->request->post['paystack_status'];
+        if (isset($this->request->post['payment_paystack_status'])) {
+            $data['payment_paystack_status'] = $this->request->post['payment_paystack_status'];
         } else {
-            $data['paystack_status'] = $this->config->get('paystack_status');
+            $data['payment_paystack_status'] = $this->config->get('payment_paystack_status');
         }
 
-        if (isset($this->request->post['paystack_sort_order'])) {
-            $data['paystack_sort_order'] = $this->request->post['paystack_sort_order'];
+        if (isset($this->request->post['payment_paystack_sort_order'])) {
+            $data['payment_paystack_sort_order'] = $this->request->post['payment_paystack_sort_order'];
         } else {
-            $data['paystack_sort_order'] = $this->config->get('paystack_sort_order');
+            $data['payment_paystack_sort_order'] = $this->config->get('payment_paystack_sort_order');
         }
 
         $data['header'] = $this->load->controller('common/header');
@@ -214,17 +212,16 @@ class ControllerExtensionPaymentPaystack extends controller
         if (!$this->user->hasPermission('modify', 'extension/payment/paystack')) {
             $this->error['warning'] = $this->language->get('error_permission');
         }
-        
-        $live_secret = $this->request->post['paystack_live_secret'];
-        $live_public = $this->request->post['paystack_live_public'];
-        $test_secret = $this->request->post['paystack_test_secret'];
-        $test_public = $this->request->post['paystack_test_public'];
+        $live_secret = $this->request->post['payment_paystack_live_secret'];
+        $live_public = $this->request->post['payment_paystack_live_public'];
+        $test_secret = $this->request->post['payment_paystack_test_secret'];
+        $test_public = $this->request->post['payment_paystack_test_public'];
  
-        if ($this->request->post['paystack_live'] && (!$this->valid_key($live_secret, 'live', 'secret') || !$this->valid_key($live_public, 'live', 'public'))) {
+        if ($this->request->post['payment_paystack_live'] && (!$this->valid_key($live_secret, 'live', 'secret') || !$this->valid_key($live_public, 'live', 'public'))) {
             $this->error['keys'] = $this->language->get('error_live_keys');
         }
 
-        if (!$this->request->post['paystack_live'] && (!$this->valid_key($test_secret, 'test', 'secret') || !$this->valid_key($test_public, 'test', 'public'))) {
+        if (!$this->request->post['payment_paystack_live'] && (!$this->valid_key($test_secret, 'test', 'secret') || !$this->valid_key($test_public, 'test', 'public'))) {
             $this->error['keys'] = $this->language->get('error_test_keys');
         }
 
--- 3.0.x/catalog/controller/extension/payment/paystack.php
+++ 3.0.x/catalog/controller/extension/payment/paystack.php
@@ -1,46 +1,36 @@
 <?php
 class ControllerExtensionPaymentPaystack extends Controller {
-	public function index()
-    {
-        $this->language->load('extension/payment/paystack');
+	public function index() {
+        $this->load->model('checkout/order');
 
-        $data['text_testmode'] = $this->language->get('text_testmode');
+        $this->load->language('extension/payment/paystack');
+
         $data['button_confirm'] = $this->language->get('button_confirm');
 
-        $data['livemode'] = $this->config->get('paystack_live');
+        $data['text_testmode'] = $this->language->get('text_testmode');
+        $data['livemode'] = $this->config->get('payment_paystack_live');
 
-        if ($this->config->get('paystack_live')) {
-            $data['key'] = $this->config->get('paystack_live_public');
+        if ($this->config->get('payment_paystack_live')) {
+            $data['key'] = $this->config->get('payment_paystack_live_public');
         } else {
-            $data['key'] = $this->config->get('paystack_test_public');
+            $data['key'] = $this->config->get('payment_paystack_test_public');
         }
-
-        $this->load->model('checkout/order');
 
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
-        if ($order_info) {
+        $data['ref'] = uniqid('' . $this->session->data['order_id'] . '-');
+        $data['amount'] = intval($order_info['total'] * 100);
+        $data['email'] = $order_info['email'];
+        $data['callback'] = $this->url->link('extension/payment/paystack/callback', 'trxref=' . rawurlencode($data['ref']), 'SSL');
 
-            $data['ref'] = uniqid('' . $this->session->data['order_id'] . '-');
-            $data['amount'] = intval($order_info['total'] * 100);
-            $data['email'] = $order_info['email'];
-            $data['callback'] = $this->url->link('extension/payment/paystack/callback', 'trxref=' . rawurlencode($data['ref']), 'SSL');
-
-            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/paystack.tpl')) {
-                return $this->load->view($this->config->get('config_template') . '/template/payment/paystack.tpl', $data);
-            } else {
-                return $this->load->view('payment/paystack.tpl', $data);
-
-            }
-        }
+        return $this->load->view('extension/payment/paystack', $data);
     }
 
-    private function query_api_transaction_verify($reference)
-    {
-        if ($this->config->get('paystack_live')) {
-            $skey = $this->config->get('paystack_live_secret');
+    private function query_api_transaction_verify($reference) {
+        if ($this->config->get('payment_paystack_live')) {
+            $skey = $this->config->get('payment_paystack_live_secret');
         } else {
-            $skey = $this->config->get('paystack_test_secret');
+            $skey = $this->config->get('payment_paystack_test_secret');
         }
 
         $context = stream_context_create(array(
@@ -54,48 +44,6 @@ class ControllerExtensionPaymentPaystack extends Controller {
         $request = file_get_contents($url, false, $context);
         return json_decode($request, true);
     }
-    // protected function query_api_transaction_verify($reference)
-    // {
-    //     $url = 'https://api.paystack.co/transaction/verify/' . urlencode($reference);
-    //     $data = array();
-        
-    //     if ($this->config->get('paystack_live')) {
-    //         $skey = $this->config->get('paystack_live_secret');
-    //     } else {
-    //         $skey = $this->config->get('paystack_test_secret');
-    //     }
-
-    //     //open connection
-    //     $ch = curl_init();
-
-    //     //set the url, and the header
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    //     // Paystack's servers require TLSv1.2
-    //     // Force CURL to use this
-    //     if (!defined('CURL_SSLVERSION_TLSV1_2')) {
-    //         define('CURL_SSLVERSION_TLSV1_2', 6);
-    //     }
-    //     curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSV1_2);
-
-    //     curl_setopt(
-    //         $ch, CURLOPT_HTTPHEADER, [
-    //         'Authorization: Bearer ' . $skey]
-    //     );
-
-    //     //execute post
-    //     $result = curl_exec($ch);
-
-    //     //close connection
-    //     curl_close($ch);
-
-    //     if ($result) {
-    //         $data = json_decode($result, true);
-    //     }
-        
-    //     return $data;
-    // }
 
     private function redir_and_die($url, $onlymeta = false)
     {
@@ -122,10 +70,8 @@ class ControllerExtensionPaymentPaystack extends Controller {
 
             $order_info = $this->model_checkout_order->getOrder($order_id);
 
-
-
             if ($order_info) {
-                if ($this->config->get('paystack_debug')) {
+                if ($this->config->get('payment_paystack_debug')) {
                     $this->log->write('PAYSTACK :: CALLBACK DATA: ' . print_r($this->request->get, true));
                 }
 
@@ -135,10 +81,10 @@ class ControllerExtensionPaymentPaystack extends Controller {
                 $order_status_id = $this->config->get('config_order_status_id');
 
                 if (array_key_exists('data', $ps_api_response) && array_key_exists('status', $ps_api_response['data']) && ($ps_api_response['data']['status'] === 'success')) {
-                    $order_status_id = $this->config->get('paystack_approved_status_id');
+                    $order_status_id = $this->config->get('payment_paystack_approved_status_id');
                     $redir_url = $this->url->link('checkout/success');
                 } else if (array_key_exists('data', $ps_api_response) && array_key_exists('status', $ps_api_response['data']) && ($ps_api_response['data']['status'] === 'failure')) {
-                    $order_status_id = $this->config->get('paystack_declined_status_id');
+                    $order_status_id = $this->config->get('payment_paystack_declined_status_id');
                     $redir_url = $this->url->link('checkout/checkout', '', 'SSL');
                 } else {
                     $order_status_id = $this->config->get('paystack_error_status_id');
