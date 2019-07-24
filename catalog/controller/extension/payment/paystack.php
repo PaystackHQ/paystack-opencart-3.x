db964fb68d67e180f1567004e02cd87882873599
Lukman Bello <lukman@paystack.com>
2019-07-24 23:58:34 +0100

    added plugin metrics tracker

--- catalog/controller/extension/payment/paystack.php
+++ catalog/controller/extension/payment/paystack.php
@@ -1,6 +1,45 @@
 <?php
+class open_cart_paystack_plugin_tracker {
+    var $public_key;
+    var $plugin_name;
+    function __construct($plugin, $pk){
+        //configure plugin name
+        //configure public key
+        $this->plugin_name = $plugin;
+        $this->public_key = $pk;
+    }
+
+   
+
+    function log_transaction_success($trx_ref){
+        //send reference to logger along with plugin name and public key
+        $url = "https://plugin-tracker.paystackintegrations.com/log/charge_success";
+
+        $fields = [
+            'plugin_name'  => $this->plugin_name,
+            'transaction_reference' => $trx_ref,
+            'public_key' => $this->public_key
+        ];
+
+        $fields_string = http_build_query($fields);
+
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+        //execute post
+        $result = curl_exec($ch);
+        //  echo $result;
+    }
+}
+
+
 class ControllerExtensionPaymentPaystack extends Controller
-{
+{ 
     public function index()
     {
         $this->load->model('checkout/order');
@@ -87,6 +126,17 @@ class ControllerExtensionPaymentPaystack extends Controller
                 $order_status_id = $this->config->get('config_order_status_id');
 
                 if (array_key_exists('data', $ps_api_response) && array_key_exists('status', $ps_api_response['data']) && ($ps_api_response['data']['status'] === 'success')) {
+                    //PSTK Logger 
+                    if ($this->config->get('payment_paystack_live')) {
+                        $pk = $this->config->get('payment_paystack_live_public');
+                    } else {
+                        $pk = $this->config->get('payment_paystack_test_public');
+                    }
+            
+                    $pstk_logger = new open_cart_paystack_plugin_tracker('opencart-3.x',$pk);
+                    $pstk_logger->log_transaction_success($trxref);
+
+                    //----------------------
                     $order_status_id = $this->config->get('payment_paystack_order_status_id');
                     $redir_url = $this->url->link('checkout/success');
                 } elseif (array_key_exists('data', $ps_api_response) && array_key_exists('status', $ps_api_response['data']) && ($ps_api_response['data']['status'] === 'failed')) {
